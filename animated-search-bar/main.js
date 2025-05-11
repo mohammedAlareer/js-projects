9e20692d1dfa62145a2b58ef1e92e7c8448ce6a3
mohammedAlareer <areermohammed96@gmail.com>
2025-05-11 16:44:51 +0300

    Delete animated-search-bar directory

--- animated-search-bar/main.js
+++
@@ -1,9 +0,0 @@
-let containerEl = document.querySelector(".container");
-let magnifierEl = document.querySelector(".magnifier");
-
-magnifierEl.addEventListener("click" , () => {
-    containerEl.classList.toggle("active");
-});
-
-
-
